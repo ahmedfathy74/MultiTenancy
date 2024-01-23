39e3c8ec36f149e54b0ef8aa9f555ef167ea1551
ahmedfathy74 <ahmd.fathi0074@gmail.com>
2024-01-23 16:08:36 +0200

    #add MutliTenancy Solution

---
+++ MultiTenancy/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using MultiTenancy.Contracts;
+global using MultiTenancy.Settings;
+global using MultiTenancy.Services;
+global using MultiTenancy.Models;
+global using MultiTenancy.Data;
+
+
---
+++ MultiTenancy/Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiTenancy.Services
+{
+    public interface IProductService
+    {
+        Task<Product> CreatedAsync(Product product);
+        Task<Product?> GetByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetAllAsync();
+    }
+}
